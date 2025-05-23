b86238a857e1d56596d7899d72198e7c9e3bdf5a
prajwal-d14 <gat15cv080@gmail.com>
2025-05-23 16:32:09 +0530

    Update jdk.sh

--- jdk.sh
+++ jdk.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
 sudo apt update
-sudo apt install fontconfig openjdk-21-jre
+sudo apt install openjdk-17-jdk
 java -version
-openjdk version "21.0.3" 2024-04-16
-OpenJDK Runtime Environment (build 21.0.3+11-Debian-2)
-OpenJDK 64-Bit Server VM (build 21.0.3+11-Debian-2, mixed mode, sharing)
+
